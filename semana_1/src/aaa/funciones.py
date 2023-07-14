6b7db06025aee3e86e0f67a2b76e3da21b5634ce
iudh <yeho@mail.com>
2023-07-14 11:22:55 +0200

    oop - funcional

---
+++ semana_1/src/aaa/funciones.py
@@ -0,0 +1,18 @@
+def sumar(a, b):
+    """
+    Funcion para sumar
+    """
+    return a+b
+
+def restar(a, b):
+    return a-b
+
+def multiplicar(a, b):
+    return a*b
+
+def dividir(a, b):
+    return a//b
+
+
+HOLA = 9000
+
