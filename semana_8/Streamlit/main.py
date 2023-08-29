e1ccb48f6dbd6489deb19079f3ffb8b32267bb26
iudh <yeho@mail.com>
2023-08-29 10:25:00 +0200

    ultima

--- semana_8/Streamlit/main.py
+++ semana_8/Streamlit/main.py
@@ -135,6 +135,18 @@ st.markdown(html, unsafe_allow_html=True)
 st.video('https://www.youtube.com/watch?v=zS5pjxseTQM&list=RDzS5pjxseTQM&start_radio=1&ab_channel=JoanAsPoliceWoman')
 
 
+import folium
 
+from streamlit_folium import st_folium
+
+# center on Liberty Bell, add marker
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
+    
 
 
